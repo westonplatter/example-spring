cad00474ab8a67f9b9f27e7baf7e98d2f9186d96
Weston Platter <weston@choozle.com>
2017-07-13 13:54:07 -0600

    started app. it runs hello world.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Example Spring
+
+## development
+
+install all spring dependencies and the `spring` command line tool.
+
+```bash
+spring run app.groovy
+```
+
+## testing
+
+## deployment
+
+## production
+
+
---
+++ src/main/java/com/choozle/mainapp/MainappApplication.java
@@ -0,0 +1,12 @@
+package com.choozle.mainapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainappApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MainappApplication.class, args);
+	}
+}
